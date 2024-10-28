e8aaaf0e108bce233aff98b8cade93a42314ec8c
aruljenithberkmans <124584591+aruljenithberkmans@users.noreply.github.com>
2024-10-28 18:09:52 +0530

    added the sample

---
+++ USJetFuelPrice/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+namespace USJetFuelPrice
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Chart_TrackballCreated(object sender, Syncfusion.Maui.Charts.TrackballEventArgs e)
+        {
+            var info = e.TrackballPointsInfo;
+
+            foreach(var item in info)
+            {
+                var date = ((Model)item.DataItem).Date;
+                item.Label = date.ToString("dd-MMM-yyyy") + ": " + item.Label + "$";
+            }
+        }
+    }
+
+}
---
+++ USJetFuelPrice/ViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USJetFuelPrice
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> WeeklyPriceData { get; set; }
+
+        public ViewModel()
+        {
+            WeeklyPriceData = new ObservableCollection<Model>();
+            ReadCSV();
+        }
+
+        public void ReadCSV()
+        {
+            Assembly executingAssembly = typeof(App).GetTypeInfo().Assembly;
+            Stream? inputStream = executingAssembly.GetManifestResourceStream("USJetFuelPrice.Resources.Raw.data.csv");
+
+            string? line;
+            List<string> lines = new List<string>();
+
+            if (inputStream != null)
+            {
+                using StreamReader reader = new StreamReader(inputStream);
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+                lines.RemoveAt(0);
+
+                foreach (var dataPoint in lines)
+                {
+                    string[] data = dataPoint.Split(',');
+
+                    var dateDetails = data[0].Split("/");
+                    var month = int.Parse(dateDetails[0]);
+                    var day = int.Parse(dateDetails[1]);
+                    var year = int.Parse(dateDetails[2]);
+
+                    var date = new DateTime(year,month,day);
+                    var price = Convert.ToDouble(data[1]);
+
+                    WeeklyPriceData.Add(new Model { Date = date, Price = price});
+                }
+            }
+        }
+
+    }
+
+    public class Model
+    {
+        public DateTime Date {  get; set; }
+
+        public double Price {  get; set; }
+    }
+
+}
